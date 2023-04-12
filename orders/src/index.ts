ffbf94621a6c3ee90aac0a9352517f4e78c7e893
shahab namvar <shahab.namvar@desphilman.com>
2023-04-12 15:30:40 +0330

    cuncorerncy issue

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -2,6 +2,8 @@ import mongoose from 'mongoose';
 
 import { app } from './app';
 import { natsWrapper } from './nats-wrapper';
+import { TicketCreatedListener } from './events/listener/ticket-created-listener';
+import { TicketUpdatedListener } from './events/listener/ticket-updated-listener';
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
@@ -35,6 +37,9 @@ const start = async () => {
 
     process.on('SIGINT', () => natsWrapper.client.close()); // ina ham baraye hamon elati ke dar close neveshte shode neveshte shode
     process.on('SIGTERM', () => natsWrapper.client.close());
+
+    new TicketCreatedListener(natsWrapper.client).listen();
+    new TicketUpdatedListener(natsWrapper.client).listen();
 
     await mongoose.connect(process.env.MONGO_URI);
     console.log('connected to mongodb-tickets');
--- orders/src/models/ticket.ts
+++ orders/src/models/ticket.ts
@@ -1,8 +1,8 @@
 import mongoose from 'mongoose';
-import { Order } from './order';
-import { OrderStatus } from '@mrshahabtickets/common';
+import { Order, OrderStatus } from './order';
 
 interface TicketAttrs {
+  id: string;
   title: string;
   price: number;
 }
@@ -40,7 +40,11 @@ const ticketSchema = new mongoose.Schema(
 );
 
 ticketSchema.statics.build = (attrs: TicketAttrs) => {
-  return new Ticket(attrs);
+  return new Ticket({
+    _id: attrs.id,
+    title: attrs.title,
+    price: attrs.price,
+  });
 };
 
 ticketSchema.methods.isReserved = async function () {
