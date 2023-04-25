22619f0d2d926c3c05558a79d51647c40407ed8c
shahab namvar <shahab.namvar@desphilman.com>
2023-04-25 22:29:32 +0330

    init

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,16 +1,42 @@
-import buildClient from "../api/build-client";
-const LandingPage = ({ currentUser }) => {
+import Link from 'next/link';
+
+const LandingPage = ({ currentUser, tickets }) => {
   //dar response.data currentUser gharar dare az backend miad
-  return currentUser ? (
-    <h1>{currentUser.email}</h1>
-  ) : (
-    <h1>You are Not Sign In</h1>
+  const ticketList = tickets.map((ticket) => {
+    return (
+      <tr key={ticket.id}>
+        <td>{ticket.title}</td>
+        <td>{ticket.price}</td>
+        <td>
+          <Link href="/tickets/[ticketId]" as={`/tickets/${ticket.id}`}>
+            View
+          </Link>
+        </td>
+      </tr>
+    );
+  });
+
+  return (
+    <div>
+      <h1>Tickets</h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Price</th>
+            <th>Link</th>
+          </tr>
+        </thead>
+        <tbody>{ticketList}</tbody>
+      </table>
+    </div>
   );
 };
 
-LandingPage.getInitialProps = async (context) => {
-  const { data } = await buildClient(context).get("/api/users_/currentuser");
-  return data;
+LandingPage.getInitialProps = async (context, client, currentUser) => {
+  const { data } = await client.get('/api/tickets');
+
+  return { tickets: data }; //tickets ro be onvan props az bala daryaft mikonim(LandingPage)
 };
 
 export default LandingPage;
---
+++ client/pages/tickets/[ticketId].js
@@ -0,0 +1,32 @@
+import Router from 'next/router';
+import useRequest from '../../hooks/use-request';
+
+const TicketShow = ({ ticket, currentUser }) => {
+  const { doRequest, errors } = useRequest({
+    url: '/api/orders',
+    method: 'post',
+    body: {
+      ticketId: ticket.id,
+    },
+    onSuccess: (order) =>
+      Router.push('/orders/[orderId]', `/orders/${order.order.id}`),
+  });
+  return (
+    <div>
+      <h1>{ticket.title}</h1>
+      <h4>Price: {ticket.price}</h4>
+      {errors}
+      <button onClick={() => doRequest()} className="btn btn-primary">
+        Purchase
+      </button>
+    </div>
+  );
+};
+
+TicketShow.getInitialProps = async (context, client) => {
+  const { ticketId } = context.query;
+  const { data } = await client.get(`/api/tickets/${ticketId}`);
+
+  return { ticket: data }; // ticket ro be onvane props az TicketShow migirim
+};
+export default TicketShow;
