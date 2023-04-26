c99a4c7278e62ce227240e6109a356f362d311d5
shahab namvar <shahab.namvar@desphilman.com>
2023-04-26 16:43:38 +0330

    asd

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -8,7 +8,7 @@ import { ExpirationCompleteListener } from './events/listener/expiration-complet
 import { PaymentCreatedListener } from './events/listener/payment-created-listener';
 
 const start = async () => {
-  console.log('starting.....     .  ');
+  console.log('starting..... 6  .  ');
 
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
